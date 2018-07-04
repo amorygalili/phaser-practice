9ff38473c006ba5074992f8a09dbecad755d3071
Amory Galili <amory@mgemi.com>
2018-07-03 20:26:39 -0400

    Created webpack build

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+// https://hackernoon.com/webpack-3-quickstarter-configure-webpack-from-scratch-30a6c394038a
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: './assets/css/app.css'
+});
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    app: './app.js'
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      { test: /\.html$/, use: ['html-loader'] },
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },
+      //file-loader(for images)
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/media/'
+            }
+          }
+        ]
+      },
+      //file-loader(for fonts)
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    extractPlugin
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist/assets/media"),
+    stats: 'errors-only',
+    open: true,
+    port: 12000,
+    compress: true
+  },
+  devtool: 'inline-source-map'
+}
