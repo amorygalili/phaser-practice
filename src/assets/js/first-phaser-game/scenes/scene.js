c28b5af6455984382b0218513233dca68ab8a75a
Amory Galili <amorygalili@gmail.com>
2019-01-27 10:12:21 -0500

    worked on stuff

--- src/assets/js/first-phaser-game/scenes/scene.js
+++ src/assets/js/first-phaser-game/scenes/scene.js
@@ -15,24 +15,67 @@ class Scene extends Phaser.Scene {
     this.load.image('block', 'assets/media/images/block.png');
     // 124 x 126
     this.load.image('block2', 'assets/media/images/block2.png');
+    //142 x 127 bricks
+    this.load.image('bricks', 'assets/media/images/bricks.png');
+    //298 × 500 tv
+    this.load.image('tv', 'assets/media/images/tv.png');
+    //400 × 32 platform
+    this.load.image('platform', 'assets/media/images/platform.png');
+    this.load.image('cabinet', 'assets/media/images/cabinet.png');
+    this.load.spritesheet('dude', 'assets/media/images/dude.png', { frameWidth: 32, frameHeight: 48 });
   }
 
   create() {
 
-    //World boundary
+    /* ========== WORLD BOUNDARY ========== */
     this.matter.world.setBounds(0, 0, 800, 600, 32, true, true, false, true);
 
+    /* ========== DRAGGABLE ITEMS ========== */
     const canDrag = this.matter.world.nextGroup();
 
-    let block = this.createBlock(1, 11, 1, 18).setCollisionGroup(canDrag)  //top
+    let block = this.createBlock(1, 11, 1, 18).setCollisionGroup(canDrag) //top
     this.createBlock(1, 0, 1, 18).setCollisionGroup(canDrag)  //ground
     this.createBlock(1, 1, 10, 1).setCollisionGroup(canDrag) //left block
     this.createBlock(18, 1, 10, 1).setCollisionGroup(canDrag)  //right block
 
-    //  Constraint on canDrag items
-    this.matter.add.mouseSpring({ length: 1, stiffness: 0.6, angularStiffness: 0,  collisionFilter: { group: canDrag } });
+    //BRICKS
+    //2nd floor middle:
+    this.matter.add.image(400, 350, 'bricks', null, { chamfer: 16 }).setScale(0.3).setBounce(0.1).setCollisionGroup(canDrag);
+    this.matter.add.image(400, 350, 'cabinet', null, { chamfer: 16 }).setScale(0.07).setBounce(0.1).setCollisionGroup(canDrag);
+    this.matter.add.image(400, 350, 'cabinet', null, { chamfer: 16 }).setScale(0.07).setBounce(0.1).setCollisionGroup(canDrag);
+    //1st floor 
+    this.matter.add.image(200, 600-40, 'bricks', null, { chamfer: 16 }).setScale(0.3).setBounce(0.1).setCollisionGroup(canDrag);
+    this.matter.add.image(300, 600-40, 'bricks', null, { chamfer: 16 }).setScale(0.3).setBounce(0.1).setCollisionGroup(canDrag);
 
-    this.splitBlock(block);
+    //FURNITURE
+    this.matter.add.image(600, 600-40, 'tv', null, { chamfer: 16 }).setScale(0.2).setBounce(0.1).setCollisionGroup(canDrag);
+
+    //STATIC FLOOR
+    this.matter.add.image(400, 400, 'platform', null, { isStatic: true }).setScale(0.174); //second floor
+
+    //PERSON
+    let person = this.matter.add.sprite(400, 450, 'dude').setScale(2);
+
+    //  Constraint on canDrag items
+    this.matter.add.mouseSpring({ length: 1, stiffness: 0.6, angularStiffness: 0,  collisionFilter: { group: canDrag } });        
+
+    // //World boundary
+    // this.matter.world.setBounds(0, 0, 800, 600, 32, true, true, false, true);
+
+    // const canDrag = this.matter.world.nextGroup();
+
+    // let block = this.createBlock(1, 11, 1, 18).setCollisionGroup(canDrag);  //top
+    // this.createBlock(1, 0, 1, 18).setCollisionGroup(canDrag)  //ground
+    // this.createBlock(1, 1, 10, 1).setCollisionGroup(canDrag) //left block
+    // this.createBlock(18, 1, 10, 1).setCollisionGroup(canDrag)  //right block
+
+    // //  Constraint on canDrag items
+    // this.matter.add.mouseSpring({ length: 1, stiffness: 0.6, angularStiffness: 0,  collisionFilter: { group: canDrag } });
+
+    let splits = this.splitBlock(block)
+    splits.forEach(split => {
+      split.setCollisionGroup(canDrag);
+    })
   }
 
   createBlock(x, y, rows, cols) {
@@ -56,15 +99,8 @@ class Scene extends Phaser.Scene {
   //output: 2 new blocks
   splitBlock(block) {
 
-    console.log('block:', block.x);
-
     let vertices = block.body.vertices;
     let boundsMin = block.body.bounds.min;
-    let boundsMax = block.body.bounds.max;
-    let width = boundsMax.x - boundsMin.y;
-    let height = boundsMax.y - boundsMin.y;
-
-    console.log('vertices:', block);
 
     let edgeCount = vertices.length;
 
@@ -93,74 +129,124 @@ class Scene extends Phaser.Scene {
     
     let verticesWithSplitPoints = [...vertices];
 
-    let splitPoint1Index = edge1 + 1;
-    let splitPoint2Index = edge2 + 2;
+    if (edge1 < edge2) {
+      var splitPoint1Index = edge1 + 1;
+      var splitPoint2Index = edge2 + 2;
+    }
+    else {
+      var splitPoint1Index = edge2 + 1;
+      var splitPoint2Index = edge1 + 2;
+    }
+
+
     verticesWithSplitPoints.splice(splitPoint1Index, 0, splitPoint1);
     verticesWithSplitPoints.splice(splitPoint2Index, 0, splitPoint2);
 
     let shape1Vertices = [];
     let shape2Vertices = [];
 
-    console.log('verticesWithSplitPoints:', verticesWithSplitPoints);
-
     for (let i = splitPoint1Index; i < splitPoint1Index + edgeCount + 3; i++) {
       if (i <= splitPoint2Index) {
         shape1Vertices.push({
-          x: verticesWithSplitPoints[i].x - boundsMin.x - width / 2,
-          y: verticesWithSplitPoints[i].y - boundsMin.y - height / 2,
+          x: verticesWithSplitPoints[i].x,
+          y: verticesWithSplitPoints[i].y,
         });
       }
       if (i >= splitPoint2Index) {
         shape2Vertices.push({
-          x: verticesWithSplitPoints[i % verticesWithSplitPoints.length].x - boundsMin.x - width / 2,
-          y: verticesWithSplitPoints[i % verticesWithSplitPoints.length].y - boundsMin.y - height / 2
+          x: verticesWithSplitPoints[i % verticesWithSplitPoints.length].x,
+          y: verticesWithSplitPoints[i % verticesWithSplitPoints.length].y
         });
       }
     }
+  
 
-    console.log('this.matter', this.matter.add);
+    let dimensions1 = this.getDimensions(shape1Vertices);
+    let dimensions2 = this.getDimensions(shape2Vertices);
 
+    let centerShape1 = Vertices.centre(shape1Vertices);
+    let centerShape2 = Vertices.centre(shape2Vertices)
+    
 
-    //let sprite = this.add.tileSprite(0, 0, 200, 200, 'block2');
-
-    /*
-    let blah = this.matter.add.image(0, 0, 'block2', null, {
-      vertices: Vertices.fromPath('40 0 40 20 100 20 100 80 40 80 40 100 0 50')
+    Vertices.translate(shape1Vertices, {
+      x: -boundsMin.x + (boundsMin.x - dimensions1.x),
+      y: -boundsMin.y + (boundsMin.y - dimensions1.y),
     });
 
-    console.log('blah:', blah);
+    Vertices.translate(shape2Vertices, {
+      x: -boundsMin.x + (boundsMin.x - dimensions2.x),
+      y: -boundsMin.y + (boundsMin.y - dimensions2.y),
+    });
 
-    */
-
-    console.log('shape1Vertices:', shape1Vertices);
-    console.log('verts:', Vertices.centre(shape1Vertices));
-
-    let center = Vertices.centre(shape1Vertices);
-
-    /*
-    Vertices.translate(shape1Vertices, {
-      x: center.x,
-      y: center.y
+    var poly = this.add.polygon(centerShape1.x, centerShape1.y, shape1Vertices, 0x000000, .5);
+    let split1 = this.matter.add.gameObject(poly, { 
+      shape: { 
+        type: 'fromVerts', 
+        verts: shape1Vertices, 
+        flagInternal: true 
+      } 
     })
-    */
-
+      .setFrictionAir(0.01)
+      .setFriction(.4)
+      .setFrictionStatic(2)
+      .setBounce(0);
+  
+    var poly2 = this.add.polygon(centerShape2.x, centerShape2.y, shape2Vertices, 0x000000, .5);
     
-    var poly = this.add.polygon(200, 0, vertices, 0xff0000, 1);
-    this.matter.add.gameObject(poly, { shape: { type: 'fromVerts', verts: vertices, flagInternal: false } }).setStatic(true);
+    console.log('poly2:', poly2);
+    
+    let split2 = this.matter.add.gameObject(poly2, { shape: { type: 'fromVerts', verts: shape2Vertices, flagInternal: true } })
+      .setFrictionAir(0.01)
+      .setFriction(.4)
+      .setFrictionStatic(2)
+      .setBounce(0)
 
+    split2.body.render.sprite.texture = 'dude';
 
+    console.log('split2:', split2.setTexture);
 
-    //var poly2 = this.add.polygon(400, 300, shape2Vertices, 0xff0000, 1);
-    //this.matter.add.gameObject(poly2, { shape: { type: 'fromVerts', verts: shape2Vertices, flagInternal: false } });
+    block.destroy();
 
-
+    return [split1, split2];
   }
 
   getRandomPointOnLine(point1, point2) {
     let rand = Math.random();
     return {
-      x: point1.x + (point2.x - point1.x) * rand,
-      y: point1.y + (point2.y - point1.y) * rand,
+      x: point2.x + (point1.x - point2.x) * rand,
+      y: point2.y + (point1.y - point2.y) * rand,
+    };
+  }
+
+  getDimensions(points) {
+    let minX = points[0].x;
+    let minY = points[0].y;
+
+    let maxX = points[0].x;
+    let maxY = points[0].y;
+
+    points.forEach((point) => {
+      if (point.x < minX) {
+        minX = point.x;
+      }
+      else if (point.x > maxX) {
+        maxX = point.x;
+      }
+
+
+      if (point.y < minY) {
+        minY = point.y;
+      }
+      else if (point.y > maxY) {
+        maxY = point.y;
+      }
+    });
+
+    return {
+      x: minX,
+      y: minY,
+      width: maxX - minX,
+      height: maxY - minY
     };
   }
 
